4516684a04cfe6342ee694b97f5b834df7e0e083
Lee <li20020410@126.com>
2023-09-30 16:13:02 +0800

    Refactor driver code

--- code/driver.py
+++ code/driver.py
@@ -1,17 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from downsampler import downsample2d, downsample1d
-from full_explicit_solver import solve
+from exact_boundary import solve_exact
+from dufort_frankel_solver import solve
 
 x_length = 10
 x_count = 1000
-y_length = 5
+y_length = 0.005
 y_count = 50
 dx = x_length / x_count
 dy = y_length / y_count
 
 nu = 1e-6
 u_inf = 10
+
+downsample_coef_x = 20
+downsample_coef_y = 2
+downsamplexy = lambda x : downsample2d(x, downsample_coef_x, downsample_coef_y)
+downsamplex = lambda x : downsample1d(x, downsample_coef_x)
 
 u = np.zeros([x_count, y_count])
 v = np.zeros([x_count, y_count])
@@ -21,16 +27,20 @@ boundary = np.zeros([x_count])
 for j in range(y_count):
     u[0][j], v[0][j] = u_inf, 0
 
-v.fill(2 * nu / dy)
 # Buttom edge
 for i in range(x_count):
     u[i][0] = v[i][0] = 0
-    v[i][1] = nu / dy
 
 solve(u, v, boundary, dx, dy, u_inf, nu)
-solve(u, v, boundary, dx, dy, u_inf, nu)
 
-U, V, B = downsample2d(u, 20, 2), downsample2d(v, 20, 2), downsample1d(boundary, 20)
+U, V = downsamplexy(u), downsamplexy(v)
 plt.quiver(np.transpose(U), np.transpose(V), scale=500)
-plt.plot(B/2)
+
+B = downsamplex(boundary)
+plt.plot(B / downsample_coef_y)
+
+#B2 = solve_exact(x_length, dx, nu, u_inf, x_count=x_count, dy=dy)
+#B2 = downsamplex(B2 / downsample_coef_y)
+#plt.plot(B2)
+
 plt.show()
